a1e1280ac97e12503ecb8fc9b1e1bb833ef39e0b
Stöffler Mario <mtf@peemdomain.at>
2022-09-16 19:06:47 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    //main
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Hello to KONF 2022!");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4");  // STRG+D
+
+
+
+    }
+
+
+}
